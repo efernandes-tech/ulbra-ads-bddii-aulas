9d2de817ffa4823be16089c68685ba7bedae95c0
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-05-21 19:50:03 -0300

    Create livro_cap5_exemplo05.sql

---
+++ livro_cap5_exemplo05.sql
@@ -0,0 +1,6 @@
+// Exemplo 5: - “Exemplo de NEXTVAL e CURRVAL”
+
+CREATE SEQUENCE exemplo_sequence;
+SELECT exemplo_sequence.NEXTVAL FROM dual;
+SELECT exemplo_sequence.CURRVAL FROM dual;
+SELECT exemplo_sequence.NEXTVAL FROM dual;
