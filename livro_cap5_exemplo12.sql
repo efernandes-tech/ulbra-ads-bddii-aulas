43bcd37ce0c028a3a0e62d846b820799e96c4441
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-05-21 21:15:45 -0300

    Rename livro_cap5_exemplo11.sql to livro_cap5_exemplo12.sql

---
+++ livro_cap5_exemplo12.sql
@@ -0,0 +1,11 @@
+// Exemplo 12: - “Inserindo novos registros”
+
+insert into TBALUNO(codigo,nome,idade,nota1,nota2,media)
+    values (5, ‘Anderson’,26, 8.5, 9.5, 6);
+
+insert into TBALUNO(nome,idade,nota1,nota2)
+    values (‘Sergio’, -25,6.1, 8.3);
+
+insert into TBALUNO(nome ,nota1,nota2)
+    values (‘Juliano’, 4.5, 5.7);
+    
